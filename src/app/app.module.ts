ecd220af91001bf9e80eed0d99f8b6b20758d78d
Sylvain MARION <s-marion@hotmail.fr>
2018-03-27 16:30:47 +0200

    Un grand pas pour l'hommethentification

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,28 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+
+
+ 
+// Import the AF2 Module
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+ 
+ 
+// AF2 Settings
+export const firebaseConfig = {
+  apiKey: "AIzaSyAkJS-OR0RG1Osb5p8xrkdCAlYlcSCInpA",
+  authDomain: "todolist-af8e0.firebaseapp.com",
+  databaseURL: "https://todolist-af8e0.firebaseio.com",
+  projectId: "todolist-af8e0",
+  storageBucket: "todolist-af8e0.appspot.com",
+  messagingSenderId: "392620180704"
+};
 
 @NgModule({
   declarations: [
@@ -14,7 +34,10 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,18 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  t: String = "ahaha";
+  constructor(public navCtrl: NavController, public afAuth: AngularFireAuth) {
 
+  }
+
+  login() {
+    this.t = "hbvvdjk";
+    this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+  }
+
+  logout() {
+    this.afAuth.auth.signOut();
   }
 
 }
